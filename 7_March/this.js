eda382f288ea03e37072c9b9e4a5126864b2bade
Talha Anwar <mtalhaanwar359@gmail.com>
2024-03-07 17:53:53 +0500

    objects

--- 7_March/this.js
+++ 7_March/this.js
@@ -1,1 +1,42 @@
-console.log("THis is a file in which we are going to study about the this keyword")
+const arrayone = [{
+    id: "Mern1",
+    name: "indexOne",
+    role: "Admin",
+    isActive: ture
+},
+
+{
+    id: Mern2,
+    name:"indexTwo",
+    rule: "user",
+    isactive: ture
+}
+
+]
+
+const objectMiddle = {
+    id: "Mern",
+    name: "IndexThree",
+    role: "user",
+    isActive: false
+}
+
+
+const objectone = {
+    name: "Javascript",
+    isActive: true,
+    isDelated: false,
+    version: "Es9"
+
+}
+
+const objectEnd = {
+    name: "End",
+    isActive: false,
+    isDeleted: tree
+}
+
+arrayone.push(objectEnd);
+arrayone.unshift(objectone);
+
+const newobj = []
